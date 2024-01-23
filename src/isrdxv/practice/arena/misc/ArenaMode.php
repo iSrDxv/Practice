5e1b4c8e829e977e5ea5a87ec28e40817118078c
srclauyt@gmail.com <srclauyt@gmail.com>
2024-01-23 14:35:28 -0300

    cositas

---
+++ src/isrdxv/practice/arena/misc/ArenaMode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace isrdxv\practice\arena\misc;
+
+interface ArenaMode
+{
+  const NO_DEBUFF = "nodebuff";
+  
+  const COMBO_FLY = "comboFly";
+  
+  const CLASSIC = "classic";
+  
+  const BRIDGE = "bridge";
+  
+  const FIST = "fist";
+  
+  const SUMO = "sumo";
+}
