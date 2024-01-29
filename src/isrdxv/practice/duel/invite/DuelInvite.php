0a8c580c07ecac34fe5b4610337122e4ee41adaa
srclauyt@gmail.com <srclauyt@gmail.com>
2024-01-29 18:18:54 -0300

    PREPARANDO: /duel y sus sistemas (queue)

---
+++ src/isrdxv/practice/duel/invite/DuelInvite.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace isrdxv\practice\duel\invite;
+
+use pocketmine\player\Player;
+
+class DuelInvite{
+
+	private string $from;
+	private string $to;
+	private string $kit;
+	private bool $ranked;
+
+	public function __construct(Player $from, Player $to, string $kit, bool $ranked){
+		$this->from = $from->getName();
+		$this->to = $to->getName();
+		$this->kit = $kit;
+		$this->ranked = $ranked;
+	}
+
+	public function getFrom(): string{
+		return $this->from;
+	}
+
+	public function getTo(): string{
+		return $this->to;
+	}
+
+	public function getKit(): string{
+		return $this->kit;
+	}
+
+	public function isRanked(): bool{
+		return $this->ranked;
+	}
+	
+}
