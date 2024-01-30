342ad7acd92012bb44abc7bf0fde966bf44588a7
iSrDxv <srclauyt@gmail.com>
2024-01-30 18:19:48 -0300

    agregar elo a la db xddd

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -2,11 +2,11 @@
 -- #{ practice
 
 -- # { init.user
-CREATE TABLE IF NOT EXISTS data_user(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30), custom_name VARCHAR(30) NULL, rank TEXT, language TEXT, coin INT, firstplayed TEXT, lastplayed TEXT, kills INT, wins INT, deaths INT, address TEXT, device TEXT, control TEXT);
+CREATE TABLE IF NOT EXISTS data_user(xuid VARCHAR(23) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30), custom_name VARCHAR(30) NULL, rank TEXT, language TEXT, coin INT, elo INT, firstplayed TEXT, lastplayed TEXT, kills INT, wins INT, deaths INT, address TEXT, device TEXT, control TEXT);
 -- # }
 
 -- # { init.settings
-CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN);
+CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(23) NOT NULL UNIQUE PRIMARY KEY, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN);
 -- # }
 
 -- # { data
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY
 -- # :rank string
 -- # :language string
 -- # :coin int
+-- # :elo int
 -- # :firstplayed string
 -- # :lastplayed string
 -- # :kills int
@@ -24,7 +25,7 @@ CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY
 -- # :address string
 -- # :device string
 -- # :control string
-INSERT INTO data_user(xuid, name, custom_name, rank, language, coin, firstplayed, lastplayed, kills, wins, deaths, address, device, control) VALUES (:xuid, :name, :custom_name, :rank, :language, :coin, :firstplayed, :lastplayed, :kills, :wins, :deaths, :address, :device, :control);
+INSERT INTO data_user(xuid, name, custom_name, rank, language, coin, elo, firstplayed, lastplayed, kills, wins, deaths, address, device, control) VALUES (:xuid, :name, :custom_name, :rank, :language, :coin, :elo, :firstplayed, :lastplayed, :kills, :wins, :deaths, :address, :device, :control);
 -- # }
 
 -- # { settings
