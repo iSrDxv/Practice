0427f3679ec03c5196baa054a03ce4e768f94019
iSrDxv <srclauyt@gmail.com>
2024-02-02 21:47:17 -0300

    prueba de cmd /duel

--- src/isrdxv/practice/command/DuelCommand.php
+++ src/isrdxv/practice/command/DuelCommand.php
@@ -27,7 +27,7 @@ class DuelCommand extends BaseCommand
   
   function __construct(PracticeLoader $loader)
   {
-      parent::__construct($loader, "hub", TextFormat::DARK_AQUA . "Send pvp request");
+      parent::__construct($loader, "duel", TextFormat::DARK_AQUA . "Send pvp request");
       $this->setAliases(["d"/*, "1vs1"*/]);
       $this->setUsage("/duel <player>");
       $this->setPermission("practice.command.duel");
@@ -35,18 +35,14 @@ class DuelCommand extends BaseCommand
 
   protected function prepare(): void
   {
-    $this->registerArgument(0, new RawStringArgument("name", true));
+    $this->registerArgument(0, new RawStringArgument("name"));
   }
   
   function onRun(CommandSender $sender, string $aliasUsed, array $args): void
   {
     if ($sender instanceof Player && ($session = SessionManager::getInstance()->get($sender)) !== null && $this->testPermissionSilent($sender)) {
-      if (array_key_exists($args["name"]) && count($args["name"]) === 0) {
-        $sender->sendForm();
-        return;
-      }
-      if (($player = Server::getInstance()->getPlayerByPrefix($name = trim(implode(" ", $args)))) !== null && $player->getName() !== $sender->getName()) {
-        $sender->sendForm();
+      if (array_key_exists($args["name"]) && ($player = Server::getInstance()->getPlayerByPrefix($name = trim(implode(" ", $args)))) !== null && $player->getName() !== $sender->getName()) {
+        $sender->sendMesage("test");
         return;
       }
       $sender->sendMessage(Practice::SERVER_PREFIX . TextFormat::RED . "Can not find player $name");
