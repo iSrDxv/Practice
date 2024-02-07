6f9df3ba9362819d2e1dc0a4f5de073fd7c94404
iSrDxv <srclauyt@gmail.com>
2024-02-07 18:28:24 -0300

    por ahora esto xd

---
+++ src/isrdxv/practice/handler/DuelHandler.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace isrdxv\practice\handler;
+
+use isrdxv\practice\{
+    Practice,
+    PracticeLoader
+};
+
+use pocketmine\player\Player;
+use pocketmine\utils\SingletonTrait;
+
+final class DuelHandler
+{
+    use SingletonTrait;
+
+    function __construct()
+    {
+        self::setInstance($this);
+    }
+}
