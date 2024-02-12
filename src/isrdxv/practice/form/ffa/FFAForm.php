d1a66916171a24ecd88ec5e6bf8bdd762aaa9546
iSrDxv <srclauyt@gmail.com>
2024-02-12 00:45:22 -0300

    FIX

--- src/isrdxv/practice/form/ffa/FFAForm.php
+++ src/isrdxv/practice/form/ffa/FFAForm.php
@@ -8,6 +8,7 @@ use dktapps\pmforms\{
     MenuOption,
     FormIcon
 };
+use isrdxv\practice\manager\ArenaManager;
 use isrdxv\practice\manager\SessionManager;
 use isrdxv\practice\Practice;
 use pocketmine\player\Player;
@@ -20,8 +21,10 @@ class FFAForm extends MenuForm
     {
         $arenas = $args[0];
         $options = [];
-        foreach($arenas as $arena) {
-            $options[] = new MenuOption(Practice::SERVER_COLOR . $arena->getName() . TextFormat::EOL . Practice::SERVER_COLOR . "Players: " . count($arena->getPlayers()), new FormIcon($arena->getIcon(), FormIcon::IMAGE_TYPE_PATH));
+        foreach($arenas as $name) {
+            if (($arena = ArenaManager::getInstance()->get($name)) !== null) {
+                $options[] = new MenuOption(Practice::SERVER_COLOR . $arena->getName() . TextFormat::EOL . Practice::SERVER_COLOR . "Players: " . count($arena->getPlayers()), new FormIcon($arena->getIcon(), FormIcon::IMAGE_TYPE_PATH));
+            }
         }
         parent::__construct("FFA Menu", "Choose the sand that you like or want the most", $options, function(Player $player, int $selectedOption) use($arenas): void {
             if (isset($arenas[$selectedOption])) {
